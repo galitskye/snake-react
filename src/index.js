be01e570bbc35298753bd46ff408a4b4a7e79aa4
galitsky.e.o <galitsky.e.o@gmail.com>
2018-01-08 13:25:30 +0200

    Fix sass and src

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './node_modules/App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './components/App';
+
+import './scss/styles.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
