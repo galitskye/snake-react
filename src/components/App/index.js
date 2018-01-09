dd1454e7a7e9f377adb7f3a2e39b11d8c75cc846
galitsky.e.o <galitsky.e.o@gmail.com>
2018-01-09 08:53:01 +0200

    begin game develop

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,44 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {
+    Row,
+    Col,
+    Button,
+    Icon
+} from 'react-materialize';
 
 class App extends Component {
-  render() {
-    return (
-      <h1>
-        Hello world
-      </h1>
-    );
-  }
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        console.log('Mount')
+    }
+
+    render() {
+        return (
+            <div>
+                <Row>
+                    <Col s={5}>
+                        <div className="game-wrapper">
+                            <canvas id="snake">HTML5 not supported!</canvas>
+                        </div>
+                    </Col>
+                    <Col s={7}>
+                        <h1>
+                            Hello Username!
+                        </h1>
+                        <h4>Welcome into "React-Snake Game"</h4>
+                        <p></p>
+                        <div>
+                            <Button className="btn-large">Start game<Icon left>directions</Icon></Button>
+                            <Button className="btn-large red lighten-3">Restart<Icon left>ev_station</Icon></Button>
+                        </div>
+                    </Col>
+                </Row>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
 
+import { createStore } from 'redux';
+
+import App from './components/App';
 import './scss/styles.css';
+
+const store = createStore(test);
+
+function test(state = [], {type, payload}) {
+    if(type === 'NEW_HEAD') {
+        return [
+            ...state,
+            payload
+        ]
+    }
+    return state;
+}
+
+store.subscribe(() => {
+     console.log('subscribe', store.getState());
+});
+
+store.dispatch({
+    type: 'NEW_HEAD',
+    payload: [1,4]
+});
+
+store.dispatch({
+    type: 'NEW_HEAD',
+    payload: [2,4]
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
